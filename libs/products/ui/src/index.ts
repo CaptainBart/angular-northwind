6dd3d05da63714143499efd6e57d018890055dd6
Bart <bkapitein@stiply.nl>
2024-01-20 16:11:50 +0100

    add ui compoents for product

--- libs/products/ui/src/index.ts
+++ libs/products/ui/src/index.ts
@@ -1,1 +1,5 @@
 export * from './lib/products-list/products-list.component';
+
+export * from './lib/products-table/products-table.component';
+
+export * from './lib/product-details/product-details.component';
---
+++ libs/products/ui/src/lib/products-table/products-table.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'nw-products-table',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './products-table.component.html',
+  styleUrl: './products-table.component.css',
+})
+export class ProductsTableComponent {}
